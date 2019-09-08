b64b4608e5dbac8e3aa3767d3d8bbbecc1d94594
luoming <luomingbear@163.com>
2019-09-08 23:26:12 +0800

    添加像素中文字体和绘制方法drawText(text,x,y,size);添加qseprite动画播放库

--- lib/compatibility.lua
+++ lib/compatibility.lua
@@ -5,3 +5,12 @@ if love._version_major >= 11 then
 		_setColor(r / 255, g / 255, b / 255, (a or 255) / 255)
 	end
 end
+
+--[[
+	绘制文本，默认使用了中文像素的字体，修复直接print，无法显示中文的问题
+]]
+function drawText(text,x,y,size)
+    local font = love.graphics.newFont("assets/fonts/fz.ttf",size)
+    love.graphics.setFont(font)
+    love.graphics.print(text,x,y)
+end
--- screens/createroom.lua
+++ screens/createroom.lua
@@ -1,31 +1,58 @@
 -- 创建房间的场景
 local class = require('lib.hump.class')
 local keys = require('lib.keys')
-local socket = require "socket"
-local pass = keys.A
+local peachy  = require('lib.peachy')
 
 local CreateRoomScreen = class{}
 
 function CreateRoomScreen:init(ScreenManager)
     self.screen = ScreenManager 
+    self.pwd=""
 end
 
 function CreateRoomScreen:activate()
     -- 先断开所有的链接
+    imgRunner2 = love.graphics.newImage("assets/images/runer2run.png")
+    runner2Left = peachy.new("assets/images/runer2run.json",imgRunner2,"Left")
 
+end
+
+function CreateRoomScreen:update(dt)
+    runner2Left:update(dt) 
 end
 
 function CreateRoomScreen:draw()
-    love.graphics.print("Create")
+    -- 绘制背景
+    love.graphics.clear(100,100,129)
+    -- 绘制提示语
+    love.graphics.setColor(50,50,100)
+    drawText("请选择↑、↓、←、→作为密码并按下A确定",15,10,16)
+    -- 绘制密码
+    love.graphics.setColor(40,40,40)
+    love.graphics.setLineWidth(2)
+    love.graphics.rectangle("fill",140,100,40,40,8,8)
+    love.graphics.setColor(80,80,80)
+    love.graphics.rectangle("line",140,100,40,40,8,8)
+    love.graphics.setColor(181,74,92)
+    drawText(self.pwd,148,96,46)
+    -- 绘制开始按钮
+    if self.pwd ~= "" then
+        love.graphics.setColor(148,134,168)
+        drawText("Start",260,210,16)
+    end
+    -- 播放动画
+    runner2Left:draw(100,100)
 end
 
 function CreateRoomScreen:keypressed(key)
-    if key == keys.A then
-        pass = keys.A
-    elseif key == keys.X then 
-        pass = keys.X
-    elseif key == keys.Y then
-        pass = keys.Y
+    if key == keys.DPad_up then
+        self.pwd = "↑"
+    elseif key == keys.DPad_down then 
+        self.pwd = "↓"
+    elseif key == keys.DPad_right then
+        self.pwd = "→"
+    elseif key == keys.DPad_left then
+        self.pwd = "←"
     elseif key == keys.B then
         --back home
         self.screen:view('/', 'reset')
